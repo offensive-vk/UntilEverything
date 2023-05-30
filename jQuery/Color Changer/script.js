6a60013b4b355b9778346606b04c40bab2c4b1af
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-30 21:49:43 +0530

    Updates

--- jQuery/Color Changer/script.js
+++ jQuery/Color Changer/script.js
@@ -1,3 +1,8 @@
+/*!
+    jQuery is a JavaScript library that provides a simple interface to easily create and manipulate jQuery objects and functions that can be used to manipulate jQuery objects and functions.
+    Basic approach is as follows:
+*/
+
 $(document).ready(function() {
     $("#btn").click(function() {
             var color = ["white", "red", "green", "yellow", "blue", "magenta", "cyan", "black", "orange"];
