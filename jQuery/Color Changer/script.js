860dd55129182ebbf8bf70bffa89742a2e4c6a9d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-05-30 22:54:00 +0530

    Final

--- jQuery/Color Changer/script.js
+++ jQuery/Color Changer/script.js
@@ -5,11 +5,11 @@
 
 $(document).ready(function() {
     $("#btn").click(function() {
-            var color = ["white", "red", "green", "yellow", "blue", "magenta", "cyan", "black", "orange"];
-            var i = Math.floor(Math.random() * 10);
+        var color = ["white", "red", "green", "yellow", "blue", "magenta", "cyan", "black", "orange"];
+        var i = Math.floor(Math.random() * 10);
 
-            $("#root").css("backgroundColor",color[i]);
-            $("title").text("Color is "+color[i]);
+        $("#root").css("backgroundColor",color[i]);
+        $("title").text("Color is "+color[i]);
     });
   });
   
