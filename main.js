7e36fd76199a7abc35dfd118621c767dd625d89d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-07 22:40:08 +0530

    #

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const src = document.images[0].src
 document.querySelectorAll("tag") // all tags
 document.querySelector("tag/class/id") // single 
-const name = "Pagani";
-const check = () => document.write(name+" ")
+
