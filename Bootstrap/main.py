92ee1d205661eab318979c3407f72a2c5bf11db3
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-09 20:40:56 +0530

    #

--- Bootstrap/main.py
+++ Bootstrap/main.py
@@ -8,35 +8,35 @@ os.system("ipconfig /all")
 
 print("Shruti is learning python now ")
 
-# name = input("Enter Your Name: ")
-# age = int(input("Enter Your Age: "))
-# res = float(input("What's your result? "))
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age: "))
+res = float(input("What's your result? "))
 
-# for i in range(1, len(name)):
-#     print(i*name," \n")
+for i in range(1, len(name)):
+    print(i*name," \n")
 
-# def rex():
-#     num = random.randint(99,9999)
-#     print(num)
-#     # print(999*999)
-# rex()
-# class example:
-#     try:
-#         print(random.randint(99,999)/0)
-#     except ZeroDivisionError:
-#         print(random.randint(10,99)/1)
-#     finally:
-#         print("Sorry vro thak gaya hu")
-# pass
-# def guess(num):
-#     number = random.randint(1, 10)
-#     guess = int(input("Guess a number between 1 and 10: "))
+def rex():
+    num = random.randint(99,9999)
+    print(num)
+    # print(999*999)
+rex()
+class example:
+    try:
+        print(random.randint(99,999)/0)
+    except ZeroDivisionError:
+        print(random.randint(10,99)/1)
+    finally:
+        print("Sorry vro thak gaya hu")
+pass
+def guess(num):
+    number = random.randint(1, 10)
+    guess = int(input("Guess a number between 1 and 10: "))
 
-#     if guess == number:
-#         print("Congratulations! You guessed the correct number.")
-#     else:
-#         print("Sorry, the number was", number)
+    if guess == number:
+        print("Congratulations! You guessed the correct number.")
+    else:
+        print("Sorry, the number was", number)
 
-# guess(444)
+guess(444)
 
-# function(guess);
+function(guess);
