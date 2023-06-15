f5baf3b2319ad6ef2354704912a27143953279fe
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-15 16:41:12 +0530

    #

--- Bootstrap/Main.java
+++ Bootstrap/Main.java
@@ -13,7 +13,7 @@ class MyThread extends Thread {
     @Override
     public void run() {
         for(int i = 0; i < 9; i++) {
-            System.out.println("Count Thread"+i+1);    
+            System.out.println("Count Thread"+i*989210);    
         }
         System.out.println("MyThread is running");
         // sleep(2000, 1000);
