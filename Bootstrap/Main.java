400980d77bf11c95b06f10c417e5d18077414dcf
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-16 12:08:02 +0530

    #

--- Bootstrap/Main.java
+++ Bootstrap/Main.java
@@ -3,4 +3,11 @@ public class Main{
     public static void main(String[] args) {
         System.exit(0);
     }
+
+    class Kid{
+        private static final String name = "Vedansh";
+        protected Kid(){
+            System.out.println(name);
+        }
+    }
 }
