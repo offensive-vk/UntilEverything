70bb3b665c2099589e5aed25c4acc901d1296ef6
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-16 12:11:19 +0530

    #

--- Bootstrap/Main.java
+++ Bootstrap/Main.java
@@ -1,13 +1,14 @@
 package Bootstrap;
 public class Main{
-    class Kid{
+
+    public static void main(String[] args) {
+        Kid bruh = new Kid();
+        
+    }
+}
+class Kid{
         private static final String name = "Vedansh";
         public Kid(){
             System.out.println(name);
         }
-    }
-    public static void main(String[] args) {
-        Kid bruh = new Kid();
-        System.out.println(bruh.name);
-    }
 }
