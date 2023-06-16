b66e0c48ebfdac702280876c8f50de707a76c8d9
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-16 12:05:02 +0530

    #

--- Bootstrap/Main.java
+++ Bootstrap/Main.java
@@ -1,28 +1,6 @@
 package Bootstrap;
 public class Main{
     public static void main(String[] args) {
-        MyThread thread = new MyThread();
-        
-        thread.start();
-        for(int i = 0; i < 9; i++) {
-            System.out.println("Count "+i);
-        }
-    }
-}
-class MyThread extends Thread {
-    @Override
-    public void run() {
-        for(int i = 0; i < 9; i++) {
-            System.out.println("Count Thread"+i*989210);    
-        }
-        System.out.println("MyThread is running");
         
     }
 }
-class MyRunnable implements Runnable {
-    public void run(){
-        System.out.println("Count");    
-    }
-}
-
-//ends
