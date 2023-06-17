bf0344d524d5c719b80bab7ee304a1da51c504b2
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-17 15:48:34 +0530

    #

--- Bootstrap/Main.java
+++ Bootstrap/Main.java
@@ -1,21 +1,7 @@
 package Bootstrap;
-
-import java.io.IOError;
-
 public class Main{
 
     public static void main(String[] args) {
-        Kid bruh = new Kid();
-        bruh.display();
+
     }
-}
-class Kid{
-        private static final String name = "Vedansh";
-        public Kid(){
-            System.out.println(name);
-        }
-        protected void display(){
-            System.out.println("The name is " + name);
-            throw new IOError(null);
-        }
 }
