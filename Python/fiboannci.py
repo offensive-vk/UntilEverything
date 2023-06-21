bed968ae9c40ab80006683853548181ee784a769
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-21 13:43:26 +0530

    $

---
+++ Python/fiboannci.py
@@ -0,0 +1,10 @@
+
+def fibonacci(n):
+    sequence = [0, 1]
+    for i in range(2, n):
+        sequence.append(sequence[i-1] + sequence[i-2])
+    return sequence
+
+user_input = int(input("Enter a number: "))
+fib_sequence = fibonacci(user_input)
+print(fib_sequence)
