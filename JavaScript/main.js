3d73be036a193cda98fb992fb9bfc2c193c7dc75
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-23 14:07:33 +0530

    node js

---
+++ JavaScript/main.js
@@ -0,0 +1,15 @@
+// Generates a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+// Prompt the user to guess the number
+let guess = Math.floor(Math.random() * 100) + 1;
+console.log(guess);
+// Convert the user's input to a number
+guess = parseInt(guess);
+
+// Check if the guess is correct and provide feedback
+if (guess === randomNumber) {
+  console.log("Congratulations! You guessed the correct number!");
+} else {
+  console.log("Sorry, that's incorrect. The number was " + randomNumber);
+}
