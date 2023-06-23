5666a5feefa0db1589e3f48d984e90fa6a6c48a7
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-23 14:11:36 +0530

    E$

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -1,13 +1,11 @@
-// Generates a random number between 1 and 100
+
 const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-// Prompt the user to guess the number
 let guess = Math.floor(Math.random() * 100) + 1;
 console.log(guess);
-// Convert the user's input to a number
-guess = randomNumber;
 
-// Check if the guess is correct and provide feedback
+guess = randomNumber; //easy
+
 if (guess === randomNumber) {
   console.log("Congratulations! You guessed the correct number!");
 } else {
