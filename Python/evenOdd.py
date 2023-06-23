802c416c4edf4bd60a9eab77a6e7af74c87baea7
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-23 13:49:35 +0530

    Final for this

--- Python/evenOdd.py
+++ Python/evenOdd.py
@@ -1,12 +1,15 @@
-start = 1
-end = 10
+# start = 1
+# end = 10
 
-print("Even numbers:")
+start = int(input("Enter Starting Number :"))
+end = int(input("Enter Ending Number :"))
+
+print("\t\t :: Even Numbers ::\n")
 for num in range(start, end + 1):
     if num % 2 == 0:
-        print(num)
+        print("[",num,"]")
 
-print("Odd numbers:")
+print("\t\t :: Odd Numbers ::\n")
 for num in range(start, end + 1):
     if num % 2 != 0:
-        print(num)
+        print("[",num,"]")
