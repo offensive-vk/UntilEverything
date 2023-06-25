2d6eada3673345f0c3b28a822ca3e6581ad6f0b0
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-25 17:38:17 +0530

    $

---
+++ PHP/ifelse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Conditional Statements</title>
+</head>
+<body>
+<?php
+    
+    $age = 25;
+    if ($age >= 18) {
+        echo "You are eligible to vote.";
+    } else {
+        echo "You are not eligible to vote.";
+    }
+?>
+
+</body>
+</html>
