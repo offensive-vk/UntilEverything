c6dc7d7fc6c165da4a8f46f59a1f5e891e39e210
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-25 17:53:42 +0530

    Final of da day

---
+++ PHP/loops.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Loops in PHP</title>
+</head>
+<body>
+<?php
+    // Example 1: For Loop
+    echo "Counting from 1 to 10 using a for loop:<br>";
+    for ($i = 1; $i <= 10; $i++) {
+        echo $i . "<br>";
+    }
+
+    // Example 2: While Loop
+    echo "Counting from 10 to 1 using a while loop:<br>";
+    $j = 10;
+    while ($j >= 1) {
+        echo $j . "<br>";
+        $j--;
+    }
+
+    // Example 3: Foreach Loop
+    $fruits = array("Apple", "Banana", "Orange");
+    echo "List of fruits:<br>";
+    foreach ($fruits as $fruit) {
+        echo $fruit . "<br>";
+    }
+?>
+
+</body>
+</html>
