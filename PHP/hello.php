e4b4a83e2f93f735679bfc39455bdbb29368e804
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-25 16:56:19 +0530

    $

---
+++ PHP/hello.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>One Div</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP";
+    ?>
+</body>
+</html>
