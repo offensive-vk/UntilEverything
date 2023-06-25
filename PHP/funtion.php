c7e9073a31626a4eb0fcd95cd80129e9fd9d4528
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-25 17:19:04 +0530

    $

---
+++ PHP/funtion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Functions in PHP</title>
+</head>
+<body>
+<?php
+    function greet($name) {
+        echo "Hello, " . $name . "! Welcome to our website.";
+    }
+
+    greet("John");
+?>
+
+</body>
+</html>
