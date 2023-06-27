79417473ca3e327dd8372663613d9fbd7568a45b
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-27 18:36:43 +0530

    DOne

---
+++ PHP/bankClass.php
@@ -0,0 +1,35 @@
+<?php
+class BankAccount {
+    private $accountNumber;
+    private $balance;
+
+    public function __construct($accountNumber) {
+        $this->accountNumber = $accountNumber;
+        $this->balance = 0;
+    }
+
+    public function deposit($amount) {
+        $this->balance += $amount;
+    }
+
+    public function withdraw($amount) {
+        if ($amount <= $this->balance) {
+            $this->balance -= $amount;
+        } else {
+            echo "Insufficient balance.\n";
+        }
+    }
+
+    public function getBalance() {
+        return $this->balance;
+    }
+}
+
+// Create a bank account object
+$account = new BankAccount("123456789");
+
+$account->deposit(1000);
+$account->withdraw(500);
+echo "Account Balance: " . $account->getBalance() . "\n";
+
+?>
