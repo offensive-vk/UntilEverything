9bb4dc3a3f55b4388155b87b5347c460b5f8ae46
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-28 19:20:06 +0530

    $$

---
+++ PHP/inheritance.php
@@ -0,0 +1,36 @@
+<?php 
+//
+class Vehicle {
+    protected $brand;
+    protected $color;
+
+    public function __construct($brand, $color) {
+        $this->brand = $brand;
+        $this->color = $color;
+    }
+
+    public function displayInfo() {
+        echo "Brand: " . $this->brand . "<br>";
+        echo "Color: " . $this->color . "<br>";
+    }
+}
+
+class Car extends Vehicle {
+    private $numDoors;
+
+    public function __construct($brand, $color, $numDoors) {
+        parent::__construct($brand, $color);
+        $this->numDoors = $numDoors;
+    }
+
+    public function displayInfo() {
+        parent::displayInfo();
+        echo "Number of Doors: " . $this->numDoors . "<br>";
+    }
+}
+
+// Usage:
+$car = new Car("Nissan GTR", "Blue", 2);
+$car->displayInfo();
+
+?>
