1cb411355c8000454e14333be0a7f4b84b1f6944
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-06-29 12:39:28 +0530

    $$

--- PHP/Form Handling/submit.php
+++ PHP/Form Handling/submit.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $data = "Name: $name\nEmail: $email\nFeedback: $feedback\n";
     file_put_contents('form_data.txt', $data, FILE_APPEND);
-
+    
     // Redirect to a thank you page or display a success message
     header('Location: index.html');
     exit();
