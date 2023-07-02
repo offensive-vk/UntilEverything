af226c61a7c3c04895f9580a03725f08deeb789b
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-02 13:06:59 +0530

    js

--- JavaScript/Basic/moreOperations.js
+++ JavaScript/Basic/moreOperations.js
@@ -1,6 +1,7 @@
-const person = {
-    name: "John",
-    age: 25,
+const car = {
+    name: "Dodge Charger",
+    age: 2010,
+    price: 890229,
 };
 
 if ("name" in person) {
@@ -12,7 +13,6 @@ if ("name" in person) {
 //copy a object 
 const clonedPerson = { ...person };
 console.log(clonedPerson);
-
 
 
 const propertyCount = Object.keys(person).length;
