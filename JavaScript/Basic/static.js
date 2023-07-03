e1f07b030f5fe324615a046f13474c23c9b0b0c1
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-03 08:59:00 +0530

    class

---
+++ JavaScript/Basic/static.js
@@ -0,0 +1,12 @@
+class MathUtils {
+    static square(x) {
+        return x * x;
+    }
+
+    static cube(x) {
+        return x * x * x;
+    }
+}
+
+console.log(MathUtils.square(5)); // Output: 25
+console.log(MathUtils.cube(3)); // Output: 27
