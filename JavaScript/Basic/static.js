6bb60a8e82b36e167d82aaa9fc90dbcbec139bb7
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-03 09:02:28 +0530

    last

--- JavaScript/Basic/static.js
+++ JavaScript/Basic/static.js
@@ -10,3 +10,32 @@ class MathUtils {
 
 console.log(MathUtils.square(5)); // Output: 25
 console.log(MathUtils.cube(3)); // Output: 27
+
+
+/* 
+Another example for static
+*/
+
+class StringUtils {
+    static reverseString(string) {
+        return string.split('').reverse().join('');
+    }
+
+    static countOccurrences(string, target) {
+        let count = 0;
+        let index = string.indexOf(target);
+
+        while (index !== -1) {
+            count++;
+            index = string.indexOf(target, index + 1);
+        }
+
+        return count;
+    }
+}
+
+const reversed = StringUtils.reverseString("Hello, World!");
+console.log(reversed); // Output: "!dlroW ,olleH"
+
+const occurrences = StringUtils.countOccurrences("abracadabra", "a");
+console.log(occurrences); // Output: 5
