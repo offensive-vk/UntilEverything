16bf3f7936095885b48c73b81bcbd87c53f4b636
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-04 13:09:41 +0530

    dom

---
+++ JavaScript/DOM/getElement.js
@@ -0,0 +1,13 @@
+// Get element by ID
+const elementById = document.getElementById('myElementId');
+
+// Get elements by class name
+const elementsByClassName = document.getElementsByClassName('myClassName');
+
+// Get elements by tag name
+const elementsByTagName = document.getElementsByTagName('div');
+
+// Query selector
+const elementByQuerySelector = document.querySelector('#myElementId');
+const elementsByQuerySelectorAll = document.querySelectorAll('.myClassName');
+
