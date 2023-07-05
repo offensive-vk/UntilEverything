1c398e67938cd8391afa34a3e59ac3960420c0c5
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-05 14:12:03 +0530

    dom

---
+++ JavaScript/DOM/modification.js
@@ -0,0 +1,9 @@
+const ElementById = document.getElementById('element');
+// Modify styles
+elementById.style.backgroundColor = 'red';
+elementById.style.fontSize = '20px';
+
+// Add, remove, or toggle classes
+elementById.classList.add('newClass');
+elementById.classList.remove('oldClass');
+elementById.classList.toggle('active');
