c11b0e289868593a153a8a65c5e69066ebc3314a
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-05 14:19:05 +0530

    dom

---
+++ JavaScript/DOM/events.js
@@ -0,0 +1,9 @@
+// Get the element by ID
+const button = document.getElementById('myButton');
+
+// Add a click event listener
+button.addEventListener('click', function () {
+    // Code to be executed when the button is clicked
+    console.log('Button clicked!');
+});
+
