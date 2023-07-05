ebb84e5d10a42a7c530898bf1da542c811a48e60
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-05 14:24:16 +0530

    dom

---
+++ JavaScript/DOM/traversing.js
@@ -0,0 +1,10 @@
+// Access parent and child elements
+const parentElement = elementById.parentNode;
+const firstChild = elementById.firstChild;
+const nextSibling = elementById.nextSibling;
+
+// Find elements based on relationships or criteria
+const nextElement = elementById.nextElementSibling;
+const previousElement = elementById.previousElementSibling;
+const elementBySelector = elementById.querySelector('.childElement');
+const elementsBySelectorAll = elementById.querySelectorAll('.childElements');
