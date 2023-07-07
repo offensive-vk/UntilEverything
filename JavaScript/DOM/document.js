abeb646c0399db6d34d8a6ec6e50c0f6e29f20f2
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-07 13:20:54 +0530

    dom

---
+++ JavaScript/DOM/document.js
@@ -0,0 +1,9 @@
+console.log('Root element:', document.documentElement);
+console.log('Body element:', document.body);
+console.log('Head element:', document.head);
+console.log('Document title:', document.title);
+console.log('Document URL:', document.URL);
+console.log('Document domain:', document.domain);
+console.log('Referring document:', document.referrer);
+console.log('Document cookies:', document.cookie);
+console.log('Forms in the document:', document.forms);
