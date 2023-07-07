5be6db39dcc0d81102bc9ab13f24bf9ed8782561
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-07 12:57:42 +0530

    all events

--- JavaScript/DOM/event.js
+++ JavaScript/DOM/event.js
@@ -10,3 +10,27 @@ document.addEventListener('click', function (event) {
 });
 
 //more
+document.addEventListener('click', function (event) {
+    console.log('Event type:', event.type);
+    console.log('Target element:', event.target);
+    console.log('Current phase:', event.eventPhase);
+    console.log('Timestamp:', event.timeStamp);
+    console.log('Bubbles:', event.bubbles);
+    console.log('Cancelable:', event.cancelable);
+    console.log('Default prevented:', event.defaultPrevented);
+
+    if (event.type === 'keydown') {
+        console.log('Key code:', event.keyCode || event.which);
+        console.log('Shift key pressed:', event.shiftKey);
+        console.log('Ctrl key pressed:', event.ctrlKey);
+        console.log('Alt key pressed:', event.altKey);
+        console.log('Meta key pressed:', event.metaKey);
+    }
+
+    if (event.type === 'mousemove' || event.type === 'click') {
+        console.log('Mouse coordinates (clientX, clientY):', event.clientX, event.clientY);
+        console.log('Mouse coordinates (pageX, pageY):', event.pageX, event.pageY);
+    }
+
+    console.log('------------------------');
+});
