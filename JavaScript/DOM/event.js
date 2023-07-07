a1aad7d0eacbf6b43e23351b3ac70cda56132050
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-07 12:56:29 +0530

    event

---
+++ JavaScript/DOM/event.js
@@ -0,0 +1,11 @@
+document.addEventListener('click', function (event) {
+    var eventType = event.type;
+    var targetElement = event.target;
+    var mouseX = event.clientX;
+    var mouseY = event.clientY;
+
+    console.log('Event type:', eventType);
+    console.log('Target element:', targetElement);
+    console.log('Mouse coordinates:', mouseX, mouseY);
+});
+
