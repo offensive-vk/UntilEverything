82f39f35bbf5e77388ec9b80e2007e9ec6162ca2
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-09 12:06:51 +0530

    Object

--- JavaScript/Objects/Basic.js
+++ JavaScript/Objects/Basic.js
@@ -1,14 +1,16 @@
 
-//Creating object by new Object() constructor.
+//1. Creating object by new Object() constructor.
 
 var Student = new Object();
 Student.name = 'Manish';
 Student.rollno = '0235';
 Student.age = 23;
 
-//Creating object assigning them to a variable
+//2. Creating object assigning them to a variable
 const Cars = {
     brand: 'Audi',
     model: 'RS6',
     cost: 9032001
 };
+
+//3. Creating object by a function
