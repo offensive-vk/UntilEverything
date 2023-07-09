0791281ea2aeb9b10944f605556b2d55d75dd0b8
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-09 12:06:23 +0530

    Obj

--- JavaScript/Objects/Basic.js
+++ JavaScript/Objects/Basic.js
@@ -6,3 +6,9 @@ Student.name = 'Manish';
 Student.rollno = '0235';
 Student.age = 23;
 
+//Creating object assigning them to a variable
+const Cars = {
+    brand: 'Audi',
+    model: 'RS6',
+    cost: 9032001
+};
