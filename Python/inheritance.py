c049d8f61e748b2937d6ac4c4a43584880447d64
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-19 16:00:12 +0530

    oop

---
+++ Python/inheritance.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def make_sound(self):
+        return "Some generic sound"
+
+class Dog(Animal):
+    def make_sound(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def make_sound(self):
+        return "Meow!"
+
+# Creating instances of derived classes
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+
+print(dog.name)  # Output: Buddy
+print(dog.make_sound())  # Output: Woof!
+
+print(cat.name)  # Output: Whiskers
+print(cat.make_sound())  # Output: Meow!
