631f533a50f63ff33307bd742aa08c0f35df86af
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-19 16:47:11 +0530

    oop

--- Python/typesOf.py
+++ Python/typesOf.py
@@ -14,3 +14,21 @@ class Dog(Animal):
 dog = Dog()
 dog.speak()  # Output: Dog barks
 
+# Multiple Inheritance: Program to demonstrate multiple inheritance
+
+# Base classes
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Bird:
+    def speak(self):
+        print("Bird chirps")
+
+# Derived class with multiple inheritance
+class Parrot(Animal, Bird):
+    pass
+
+# Usage
+parrot = Parrot()
+parrot.speak()  # Output: Animal speaks
