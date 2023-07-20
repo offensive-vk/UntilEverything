316746d014e2a1013cab1c7fa3f13f1530c6e5a2
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-20 18:49:33 +0530

    for

--- Python/forLoops.py
+++ Python/forLoops.py
@@ -9,3 +9,10 @@ message = "Hello, Python!"
 print("\nUsing for loop with a string:")
 for char in message:
     print(char)
+
+# Specifying a range
+#for variable_name in range(start, end):
+
+print("\nUsing for loop with a range:")
+for num in range(1, 10):
+    print(num)
