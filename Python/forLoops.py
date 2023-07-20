1f7c29eff0aeef1a67b269c9ef5beb4a5425a68b
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-20 18:48:26 +0530

    for

---
+++ Python/forLoops.py
@@ -0,0 +1,11 @@
+
+#Simple Array For Loop
+fruits = ["apple", "banana", "orange", "grape"]
+print("Using for loop with a list:")
+for fruit in fruits:
+    print(fruit)
+
+message = "Hello, Python!"
+print("\nUsing for loop with a string:")
+for char in message:
+    print(char)
