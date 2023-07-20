a19df8e2c6b1f26dcf757bfacba312763071aa0e
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-20 18:52:18 +0530

    for

--- Python/forLoops.py
+++ Python/forLoops.py
@@ -23,3 +23,9 @@ person = {"name": "John Wick", "age": 30, "occupation": "Engineer"}
 print("\nUsing for loop with a dictionary:")
 for key, value in person.items():
     print(f"{key}: {value}")
+
+# Nesting
+print("\nUsing nested for loop:")
+for i in range(1, 4):
+    for j in range(1, 4):
+        print(f"{i} * {j} = {i * j}")
