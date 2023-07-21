36d5f40e22699244184ff01326a1d9d330d6eba9
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-21 18:02:49 +0530

    dict

--- Python/dict.py
+++ Python/dict.py
@@ -19,4 +19,21 @@ del person["occupation"]
 
 print(person)
 
-###### Basic ################
+###### Basic Finished ################
+# Student Database using dictionaries
+
+students = {
+    "John": {"age": 20, "major": "Computer Science", "GPA": 3.7},
+    "Rahul": {"age": 21, "major": "Mathematics", "GPA": 3.9},
+    "Alexei": {"age": 19, "major": "Physics", "GPA": 3.5}
+}
+
+def display(name):
+    if name in students:
+        info = students[name]
+        print(f"Name: {name}\nAge: {info['age']}\nMajor: {info['major']}\nGPA: {info['GPA']}")
+    else:
+        print(f"{name} is not in the database.")
+
+display("Rahul")
+display("Alexei")
