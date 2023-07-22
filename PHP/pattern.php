4a4b07c56bdc77465819b4afe3f52ad02ddc435a
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-22 15:08:44 +0530

    patterns

---
+++ PHP/pattern.php
@@ -0,0 +1,26 @@
+<?php
+
+$lines = 7;
+
+for ($i = 1; $i <= $lines; $i++) {
+    $count = (2 * $i) + 1;
+
+    for ($k = 1; $k <= ($lines - $i); $k++) {
+        echo str_repeat("&", 1);
+    }
+
+    $j = 1;
+
+    for ($j; $j <= $count / 2; $j++) {
+        echo $j;
+    }
+
+    $j--;
+
+    for ($k = ($j - 1); $k >= 1; $k--) {
+        echo $k;
+    }
+    echo "<br>";
+}
+
+?>
