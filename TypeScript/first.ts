8c30c757f398be1d53a8a88c76acd32638eaa4e9
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-23 11:37:05 +0530

    tsx

---
+++ TypeScript/first.ts
@@ -0,0 +1,11 @@
+
+console.log("\n\t First Typescript Program !!\n\t");
+console.log("================================\n");
+
+// Syntax for type declarations: [keyword] [varName]: [type] = [value];
+let log = console.log;
+let no: any; // type definition
+let rollno: number = 10922;
+
+log("1st no : "+no);
+log("2nd rollno : "+rollno);
