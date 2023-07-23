39459af4b0265571fa2d16221ce29044ae5158bb
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-23 15:21:31 +0530

    if else

---
+++ TypeScript/conditionals.js
@@ -0,0 +1,18 @@
+/*
+* Copyright (c) by Vedansh
+*/
+var log = console.log;
+log("\n\t Second Typescript Program !!\n\t");
+log("================================\n");
+var num1 = 423423;
+var num2 = 103423;
+var res = num1 * num2 + num1 / num2;
+if (res <= 9999) {
+    log("less than equal to 9999");
+}
+else if (res >= 9999) {
+    log("greater than equal to 9999");
+}
+log("Result is " + res);
+log("================================\n");
+log("\n End of Program !!\n\t");
---
+++ TypeScript/conditionals.ts
@@ -0,0 +1,19 @@
+/* 
+* Copyright (c) by Vedansh 
+*/
+const log = console.log;
+log("\n\t Second Typescript Program !!\n\t");
+log("================================\n");
+
+let num1 : number = 423423;
+let num2 : number = 103423;
+let res: number = num1 * num2 + num1 / num2;
+
+    if(res <= 9999){
+        log("less than equal to 9999");
+    }else if(res >= 9999){
+        log("greater than equal to 9999");
+    }
+log("Result is " + res);
+log("================================\n");
+log("\n End of Program !!\n\t");
