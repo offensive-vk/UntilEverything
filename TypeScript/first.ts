6029d2547b725888db63644db87dade891335d8c
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-23 11:38:55 +0530

    ts

--- TypeScript/first.ts
+++ TypeScript/first.ts
@@ -6,6 +6,11 @@ console.log("================================\n");
 let log = console.log;
 let no: any; // type definition
 let rollno: number = 10922;
+let string: string = "Its me rahul";
+let valid: boolean = true;
+let x: null = null;
+let y: undefined;
+let z:void;
 
 log("1st no : "+no);
 log("2nd rollno : "+rollno);
