197c8f19596eb5a43560bc74a3adfc9dd70d0577
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-23 15:22:59 +0530

    if else

--- TypeScript/conditionals.js
+++ TypeScript/conditionals.js
@@ -6,12 +6,15 @@ log("\n\t Second Typescript Program !!\n\t");
 log("================================\n");
 var num1 = 423423;
 var num2 = 103423;
-var res = num1 * num2 + num1 / num2;
+var res = num1 * num2 + num1 / num2 - num1 % num2;
 if (res <= 9999) {
     log("less than equal to 9999");
 }
 else if (res >= 9999) {
     log("greater than equal to 9999");
+}
+else {
+    log("Undesired Result lol!");
 }
 log("Result is " + res);
 log("================================\n");
