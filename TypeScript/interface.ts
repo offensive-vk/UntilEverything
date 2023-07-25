b9cc02e1407fb28209d170f5bd081ea47ba0d541
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-25 19:20:34 +0530

    ts

--- TypeScript/interface.ts
+++ TypeScript/interface.ts
@@ -7,5 +7,5 @@ function greet(person: Person) {
     console.log(`Hello, ${person.name}! You are ${person.age} years old.`);
 }
 
-const john = { name: "John", age: 25, occupation: "Engineer" };
-greet(john); // Output: Hello, John! You are 25 years old.
+const john = { name: "John Wick", age: 25, occupation: "Engineer" };
+greet(john);
