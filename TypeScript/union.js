e718b3f115b11ee9f70db0cbc0016ab711acb5d6
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-26 14:02:34 +0530

    Create union.js

---
+++ TypeScript/union.js
@@ -0,0 +1,7 @@
+// TypeScript supports union types, which allow a variable or parameter to accept multiple types. In this example, the display function can accept either a string or a number, but not a boolean.
+function display(value) {
+    console.log(value);
+}
+display("TypeScript is easy to learn");
+display(142);
+//display(true); // Error: Argument of type 'boolean'.
