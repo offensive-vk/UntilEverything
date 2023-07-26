bd70151197c7b44fdfb6905f23dea92ba2be434f
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-26 14:20:25 +0530

    extends

---
+++ TypeScript/unionAnimals.ts
@@ -0,0 +1,25 @@
+// Base interface for Animal
+interface Animal {
+    name: string;
+    sound: string;
+}
+
+// Derived interfaces using union types
+interface Dog extends Animal {
+    kind: "dog";
+}
+
+interface Cat extends Animal {
+    kind: "cat";
+}
+
+// Usage example
+function makeSound(animal: Dog | Cat) {
+    console.log(`${animal.name} says ${animal.sound}`);
+}
+
+const dog: Dog = { kind: "dog", name: "Buddy", sound: "Woof!" };
+const cat: Cat = { kind: "cat", name: "Whiskers", sound: "Meow!" };
+
+makeSound(dog); // Output: Buddy says Woof!
+makeSound(cat); // Output: Whiskers says Meow!
