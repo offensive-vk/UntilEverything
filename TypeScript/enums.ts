55b26db28dc908919d0d80c339e11f146743755e
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-27 17:20:33 +0530

    enums

--- TypeScript/enums.ts
+++ TypeScript/enums.ts
@@ -2,6 +2,7 @@
 Enumerations, often referred to as enums, are a feature in TypeScript that allows you to define a set of named constants. Enums make it easier to work with a group of related values and improve code readability by giving meaningful names to these constants.
 */
 
+// Numeric enums assign increasing numeric values to each member starting from 0 
 enum Direction {
     North, // 0
     East,  // 1
@@ -11,3 +12,13 @@ enum Direction {
 
 let myDirection: Direction = Direction.East;
 console.log(myDirection); // Output: 1
+
+// String enums allow you to assign string values to enum members.
+enum Color {
+    Red = "RED",
+    Green = "GREEN",
+    Blue = "BLUE",
+  }
+  
+  let myColor: Color = Color.Green;
+  console.log(myColor); // Output: "GREEN"  
