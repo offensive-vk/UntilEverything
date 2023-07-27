841492d0f249b7e2a0c103befd7d894b5048ce0c
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-27 17:38:17 +0530

    log

--- TypeScript/console.js
+++ TypeScript/console.js
@@ -1,37 +1,28 @@
 function consoleFunctionsDemo() {
-
     console.log("This is a log message.");
     console.info("This is an info message.");
     console.warn("This is a warning message.");
     console.error("This is an error message.");
-
-    const name = "John Bhaiya";
-    const age = 55;
-
+    var name = "John Bhaiya";
+    var age = 55;
     console.log("Hello, %s! You are %d years old.", name, age);
-
-    const fruits = ["apple", "banana", "orange"];
+    var fruits = ["apple", "banana", "orange"];
     console.table(fruits);
-
-    const person = { name: "Alice", age: 25, city: "New York" };
+    var person = { name: "Alice", age: 25, city: "New York" };
     console.dir(person);
-
     console.group("Grouping example");
     console.log("This message is inside a group.");
     console.log("So is this one.");
     console.groupEnd();
-
-    var sum: number;
+    var sum;
     console.time("Time measurement");
-    for (let i = 0; i < 1000000; i++) {
-        sum = i*1/10;
+    for (var i = 0; i < 1000000; i++) {
+        sum = i * 1 / 10;
     }
     console.log("Result: " + sum);
     console.timeEnd("Time measurement");
-
     console.assert(2 + 2 === 5, "Assertion failed! 2 + 2 is not 5.");
-
-    //console.clear();
+    
+    //console.clear(); // clears the console
 }
-
 consoleFunctionsDemo();
