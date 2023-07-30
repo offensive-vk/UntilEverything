a5daf24a80c7d143ae00b9eae2af3edd6cc5df7f
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-07-30 19:58:50 +0530

    tsc adv

---
+++ TypeScript/abstract.ts
@@ -0,0 +1,43 @@
+// This interface defines the properties and methods that all animals must have.
+interface Animal {
+    name: string;
+    speak(): void;
+}
+
+// This abstract class implements the Animal interface and defines a method called eat().
+abstract class AbstractAnimal implements Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+    speak(){
+        
+    };
+        
+    // This method is abstract and must be implemented in subclasses.
+    abstract eat(): void;
+}
+
+// This class extends the AbstractAnimal class and implements the eat() method.
+class Dog extends AbstractAnimal {
+    eat() {
+        return `${this.name} is eating!`;
+    }
+    speak() {
+        // console.log(`${this.name} is speaking !`);
+        return `${this.name} is speaking !`;
+
+    }
+}
+
+// This function creates an instance of the Dog class and calls the speak() and eat() methods.
+function main() {
+    const dog = new Dog("Raccoon");
+    console.log(dog.speak());
+    console.log(dog.eat());
+}
+main();
+// This is the output of the program.
+// Woof!
+// Spot is eating!
