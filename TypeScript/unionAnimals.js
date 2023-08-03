00fd649753692a20e7202487e360cf9926183868
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-03 19:06:08 +0530

    fix file not loading issue

--- TypeScript/unionAnimals.js
+++
@@ -1,8 +0,0 @@
-// Usage example
-function makeSound(animal) {
-    console.log("".concat(animal.name, " says ").concat(animal.sound));
-}
-var dog = { kind: "dog", name: "Buddy", sound: "Woof!" };
-var cat = { kind: "cat", name: "Whiskers", sound: "Meow!" };
-makeSound(dog); // Output: Buddy says Woof!
-makeSound(cat); // Output: Whiskers says Meow!
