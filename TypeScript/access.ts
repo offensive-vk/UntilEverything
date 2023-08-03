d1ca098e29c4a6615dbc7e97ee0f7e1ecb58168d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-03 18:40:28 +0530

    access

--- TypeScript/access.ts
+++ TypeScript/access.ts
@@ -1,7 +1,28 @@
+// class Greeter {
+//     public greet() {
+//         console.log("hi!");
+//     }
+// }
+
+
+
 class Greeter {
     public greet() {
-        console.log("hi!");
+        console.log("Hello, " + this.getName());
+    }
+    
+    protected getName() {
+        return "hi";
     }
 }
 const g = new Greeter();
 g.greet();
+class SpecialGreeter extends Greeter {
+    public howdy() {
+        // OK to access protected member here
+        console.log("Howdy, " + this.getName());
+    }
+}
+const s = new SpecialGreeter();
+s.greet(); // OK
+s.getName(); // error
