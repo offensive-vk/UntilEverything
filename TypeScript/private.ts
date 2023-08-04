0d72914d59d424d0b68b000b943fdba382946844
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-04 09:45:02 +0530

    ts

--- TypeScript/private.ts
+++ TypeScript/private.ts
@@ -1,12 +1,16 @@
 class MySafe {
     private secretKey = 12345;
     static pubKey: number = 81023;
-
+    #money = 0;
+    showMoney(): any{
+        return `${typeof(this.#money)}`;
+    }
 }
 
 const s = new MySafe();
 // console.log(s.secretKey); Property 'secretKey' is private and only accessible within class 'MySafe'.
 // OK to access by index method
 console.log(s["secretKey"]);
+console.log(s.showMoney());
 
 /*Static variables | methods  in typescript */ 
