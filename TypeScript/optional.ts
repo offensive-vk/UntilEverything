9abfb0d11bcb80faf5e62d9a07b71bb1e9a1939c
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-06 18:39:07 +0530

    ts

---
+++ TypeScript/optional.ts
@@ -0,0 +1,11 @@
+
+function greet(name: string, greeting?: string) {
+    if (greeting) {
+        console.log(`${greeting}, ${name}!`);
+    } else {
+        console.log(`Hello, ${name}!`);
+    }
+}
+
+greet("John");
+greet("Jane", "Hi"); 
