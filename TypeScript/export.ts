b1e829a5cb6674e017a862948b79ab2778eb7f3d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-10 18:32:28 +0530

    ts

---
+++ TypeScript/export.ts
@@ -0,0 +1,14 @@
+// moduleA.ts
+export const variableA = "This is variable A.";
+
+const variableB = "This is variable B.";
+
+export function functionA() {
+    console.log("Function A is called.");
+}
+
+function functionB() {
+    console.log("Function B is called.");
+}
+
+export default functionB;
