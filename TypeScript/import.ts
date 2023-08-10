77014bfcf1d88a073ea3306ff2b85bc30343ce40
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-10 18:45:22 +0530

    ts

--- TypeScript/import.ts
+++ TypeScript/import.ts
@@ -2,7 +2,9 @@
 
 import { variableA, functionA } from './export';
 import functionB from './export';
+import helloWorld from './export';
 
 console.log(variableA); 
 functionA(); 
 functionB();
+helloWorld();
