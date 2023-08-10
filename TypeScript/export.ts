5af02674e45a7653019e15f6db71bc70d0fae23d
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-10 18:46:11 +0530

    ts

--- TypeScript/export.ts
+++ TypeScript/export.ts
@@ -14,3 +14,14 @@ export default function helloWorld() {
     console.log("Hello, world from another file !");
 }
 export default functionB;
+
+export var pi = 3.14;
+export let squareTwo = 1.41;
+export const phi = 1.61;
+
+export class RandomNumberGenerator { }
+
+export function absolute(num: number) {
+    if (num < 0) return num * -1;
+    return num;
+}
