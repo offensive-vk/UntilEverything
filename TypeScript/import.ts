e8823aee1d8b0c6e3f7dfa2a231c0f9136545f17
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-10 18:33:44 +0530

    ts

---
+++ TypeScript/import.ts
@@ -0,0 +1,8 @@
+// Imported the exported functions and vars
+
+import { variableA, functionA } from './export';
+import functionB from './export';
+
+console.log(variableA); 
+functionA(); 
+functionB();
