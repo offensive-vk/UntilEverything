8060c11e53d0a64c5bb3356c5f1a102f73f183f7
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-12 10:03:20 +0530

    py

--- Python/class.py
+++ Python/class.py
@@ -7,10 +7,11 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        self.show();
+        self.show()
     
     def introduce(self):
         return f"Hello, my name is {self.name}, and I am {self.age} years old."
+
 
 person1 = Person("John", 30)
 person2 = Person("Bob", 25)
