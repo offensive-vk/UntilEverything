dc51e7ea8d58712a2ebdb4a67a713844843972ca
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-16 21:28:39 +0530

    js

---
+++ JavaScript/Advanced/IIFE.js
@@ -0,0 +1,13 @@
+//
+
+(function() {
+    const firstName = "John";
+    const lastName = "Wick";
+
+    function getFullName() {
+        return firstName + " " + lastName;
+    }
+
+    console.log("Hello from JavaScript IIFE!");
+    console.log("Full Name:", getFullName());
+})();
