f8adae63139e980d838affda1042911c5628fe5c
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-17 13:21:02 +0530

    Done

---
+++ JavaScript/Advanced/factoryfunction.jsx
@@ -0,0 +1,16 @@
+function createPerson(name, age, id) {
+    return {
+        name: name,
+        age: age,
+        id: id,
+        greet: function() {
+            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+        }
+    };
+}
+
+const person1 = createPerson("Ryan", 25, 101);
+const person2 = createPerson("Manish", 30, 102);
+
+person1.greet();
+person2.greet(); 
