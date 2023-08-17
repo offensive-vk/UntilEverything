5be6f4dde6b2375a864af64b2840fe939a0b94d5
Vedansh Khandelwal <121493131+offensive-vk@users.noreply.github.com>
2023-08-17 13:03:41 +0530

    js

--- JavaScript/Advanced/highorder.js
+++ JavaScript/Advanced/highorder.js
@@ -12,3 +12,16 @@ function subtract(a, b) {
 
 const result1 = applyOperation(add, 50, 30);
 const result2 = applyOperation(subtract, 100, 40);
+
+// Functional programming
+function multiplier(factor) {
+    return function(x) {
+        return x * factor;
+    };
+}
+
+const double = multiplier(2);
+const triple = multiplier(3);
+
+const result3 = double(5);
+const result4 = triple(5);
