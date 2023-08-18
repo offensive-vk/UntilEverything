8cce6c565f317ecad28abfb630e071e855bc2241
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-18 14:16:13 +0530

    Update Promises.js

---
+++ JavaScript/Advanced/Promises.js
@@ -0,0 +1,58 @@
+
+// 
+function orderPizza() {
+    console.log("Ordering a pizza...");
+
+    const orderPromise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const isPizzaAvailable = Math.random() >= 0.3;
+            if (isPizzaAvailable) {
+                console.log("Pizza is ready!");
+                resolve("Delicious Pizza");
+            } else {
+                console.log("Sorry, the pizza is not available right now.");
+                reject("No Pizza");
+            }
+        }, 2000);
+    });
+
+    return orderPromise;
+}
+
+function bakePizza(pizza) {
+    console.log(`Baking the ${pizza}...`);
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`${pizza} is baked and ready!`);
+            resolve(`${pizza} (Baked)`);
+        }, 3000);
+    });
+}
+
+function deliverPizza(pizza) {
+    console.log(`Delivering the ${pizza}...`);
+
+    return new Promise(resolve => {
+        setTimeout(() => {
+            console.log(`${pizza} is delivered! Enjoy your meal.`);
+            resolve(`${pizza} (Delivered)`);
+        }, 1500);
+    });
+}
+
+function handleError(error) {
+    console.error("Error:", error);
+}
+
+orderPizza()
+    .then(pizza => {
+        return bakePizza(pizza);
+    })
+    .then(bakedPizza => {
+        return deliverPizza(bakedPizza);
+    })
+    .then(deliveredPizza => {
+        console.log(`Promise chain completed: ${deliveredPizza}`);
+    })
+    .catch(handleError);
