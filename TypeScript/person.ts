28a52bf679094eb1247759c35f238bf28ad4a3bb
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-19 13:09:58 +0530

    Create person.ts

---
+++ TypeScript/person.ts
@@ -0,0 +1,52 @@
+
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+type _Person = User | Admin;
+
+const persons: _Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+];
+
+function isAdmin(person: _Person) {
+    return person.type === 'admin';
+}
+
+function isUser(person: _Person) {
+    return person.type === 'user';
+}
+
+function logPerson(person: _Person) {
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.name;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.name;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
+
+// In case you are stuck:
