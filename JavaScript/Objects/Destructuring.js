a11596a868336cca57590cb5d4b0cf2a137627f4
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-20 14:38:36 +0530

    obj

---
+++ JavaScript/Objects/Destructuring.js
@@ -0,0 +1,16 @@
+// Define an object
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    city: "New York"
+};
+
+// Destructuring assignment
+const { firstName, lastName, age, city } = person;
+
+// Display the destructured values
+console.log(`First Name: ${firstName}`);
+console.log(`Last Name: ${lastName}`);
+console.log(`Age: ${age}`);
+console.log(`City: ${city}`);
