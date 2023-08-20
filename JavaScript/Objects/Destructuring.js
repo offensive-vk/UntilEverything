138a658d28a14aec200c99afd2685f4588d4a55d
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-20 15:10:04 +0530

    desx

--- JavaScript/Objects/Destructuring.js
+++ JavaScript/Objects/Destructuring.js
@@ -14,3 +14,29 @@ console.log(`First Name: ${firstName}`);
 console.log(`Last Name: ${lastName}`);
 console.log(`Age: ${age}`);
 console.log(`City: ${city}`);
+
+// Complex Destructuring
+let options = {
+    size: {
+        width: 100,
+        height: 200
+    },
+    items: ["Cake", "Donut"],
+    extra: true
+};
+
+// destructuring assignment split in multiple lines for clarity
+let {
+    size: { // put size here
+        width,
+        height
+    },
+    items: [item1, item2], // assign items here
+    title = "Menu" // not present in the object (default value is used)
+} = options;
+
+console.log(title); 
+console.log(width);  
+console.log(height); 
+console.log(item1);  
+console.log(item2);  
