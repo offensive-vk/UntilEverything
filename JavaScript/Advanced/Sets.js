1a761f3828523aa142f849a267f4aef7ab3d2f67
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-21 09:30:51 +0530

    sets

---
+++ JavaScript/Advanced/Sets.js
@@ -0,0 +1,19 @@
+let set = new Set();
+
+let john = { name: "John" };
+let pete = { name: "Pete" };
+let mary = { name: "Mary" };
+
+// visits, some users come multiple times
+set.add(john);
+set.add(pete);
+set.add(mary);
+set.add(john);
+set.add(mary);
+
+// set keeps only unique values
+console.log(set.size);
+
+for (let user of set) {
+    console.log(user.name);
+}
