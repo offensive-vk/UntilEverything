8195d13bc483d44dc70f5a0f7b657685886a09f4
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-21 09:39:43 +0530

    sets done

--- JavaScript/Advanced/Sets.js
+++ JavaScript/Advanced/Sets.js
@@ -1,15 +1,16 @@
+// Sets is kinda same as Array but its functions are pretty much easy to use.
 let set = new Set();
 
 let john = { name: "John" };
-let pete = { name: "Pete" };
-let mary = { name: "Mary" };
+let peter = { name: "Peter" };
+let manish = { name: "Manish" };
 
 // visits, some users come multiple times
 set.add(john);
-set.add(pete);
-set.add(mary);
+set.add(peter);
+set.add(manish);
 set.add(john);
-set.add(mary);
+set.add(manish);
 
 // set keeps only unique values
 console.log(`The Size of Set is : ${set.size}`);
