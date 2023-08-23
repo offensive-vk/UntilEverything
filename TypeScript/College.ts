c0d73c6d537ee5ced1e16ce12724f3b7d9f1ee72
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-23 18:32:40 +0530

    new

--- TypeScript/College.ts
+++ TypeScript/College.ts
@@ -18,6 +18,7 @@ interface Department{
 class University{
     public students: Array<Student>;
     public branch: string[];
+    public departments: Array<Department>;
     public totalStudents: number | number[];
     public totalDepts: Array<number> | number;
 
@@ -26,13 +27,21 @@ class University{
         this.branch = [];
         this.totalStudents = [];
         this.totalDepts = [];
+        this.departments = [];
     }
     addStudent(student: Student){
         this.students.push(student);
         puts(`-> ${student.sname} <${student.sid}> added to University.\n`);
     }
+    addDepartment(dept: Department){
+        this.departments.push(dept);
+        puts(`-> ${dept.name} <${dept.identity}> added to University.\n`);
+    }
     countStudents(): string | Student[]{
         return `Total Number of Students in University : ${this.students.length}\n`;
+    }
+    countDepartments(): string | Student[]{
+        return `Total Number of Departments in University : ${this.departments.length}\n`;
     }
     // displayData(id: number): Student[] {
     //     return this.students.filter((id) => this.students[id] === id);
@@ -45,6 +54,13 @@ var Manish: Student = {
     section : "BCA Full Stack",
     branch: "Computer Science",
     isRegular: true,
+};
+
+var BCA: Department = {
+    name: "Bachelor of Computer Application",
+    identity : "BCA-AU-0990",
+    sections: ["Full Stack", "Data Science", "Cyber Security", "Android"],
+    totalStudents: 100,
 };
 const Apex = new University();
 Apex.addStudent(Manish);
