0207d5a1ed3712b68f6eaedd3c1456b0676019aa
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-30 18:00:41 +0530

    Update PromiseAll.js

--- JavaScript/Advanced/PromiseAll.js
+++ JavaScript/Advanced/PromiseAll.js
@@ -10,3 +10,22 @@ Promise.all(promises)
     .then(values => console.log(values))
     .catch(err => console.log(`Oops ! Error Occurred : ${err} `))
     .finally(() => console.log(`All Promises Fulfilled \n`));
+
+// Promise.allSettled() 
+let promise4 = new Promise((resolve) => setTimeout(resolve, 1000, 'Resolves after 1s'));
+let promise5 = new Promise((resolve) => setTimeout(resolve, 2000, 'Resolves after 2s'));
+let promise6 = new Promise((resolve, reject) => setTimeout(reject, 0, 'Rejects immediately'));
+
+let _promises = [promise4, promise5, promise6];
+
+Promise.allSettled(promises)
+    .then(results => {
+        results.forEach(result => {
+            if (result.status === 'fulfilled') {
+                console.log(result.value);
+            } else {
+                console.log(`Rejected: ${result.reason}`);
+            }
+        });
+    })
+    .finally(() => console.log(`Promises Fulfilled \n`));
