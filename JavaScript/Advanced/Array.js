8a6411a27d67b16b65673400e16e967609659715
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-30 17:45:16 +0530

    Update Array.js

--- JavaScript/Advanced/Array.js
+++ JavaScript/Advanced/Array.js
@@ -7,3 +7,9 @@ console.log(firstFruit); // "Apple"
 // Replace all occurrences of "Apple" with "Pear".
 fruits.replaceAll("Apple", "Pear");
 console.log(fruits);
+
+const numbers = [1, 2, 3, 4, 5];
+
+// Get the element at index 3.
+const thirdNumber = numbers.at(-1);
+console.log(thirdNumber); 
