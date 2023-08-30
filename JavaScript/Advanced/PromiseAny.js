f374a6fcfc25220fc04bdbb93fce96dda332e652
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-30 18:08:04 +0530

    PromiseAny

--- JavaScript/Advanced/PromiseAny.js
+++ JavaScript/Advanced/PromiseAny.js
@@ -1,4 +1,8 @@
-// Promise.any()
+/* 
+* @description: Promise.any()
+It is used to handle a collection of promises and returns a new promise that is fulfilled as soon as any of the input promises is fulfilled. If all the input promises are rejected, the returned promise will be rejected with an AggregateError containing all the rejection reasons.
+
+*/
 
 let promise1 = new Promise((resolve) => setTimeout(resolve, 1000, 'Resolves after 1s'));
 let promise2 = new Promise((resolve) => setTimeout(resolve, 2000, 'Resolves after 2s'));
@@ -7,7 +11,7 @@ let promise3 = new Promise((resolve, reject) => setTimeout(reject, 0));
 let promises = [promise1, promise2, promise3];
 
 Promise.any(promises)
-    .then(value => console.log(value)); // Resolves after 100ms
+    .then(value => console.log(value)); // Resolves after 1s
 
 // Different Method: 
 (async () => {
