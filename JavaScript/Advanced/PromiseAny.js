f3e57d2ab8e35412c445a13a735f40e8da75bbe3
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-30 17:48:52 +0530

    Promise

---
+++ JavaScript/Advanced/PromiseAny.js
@@ -0,0 +1,22 @@
+
+let promise1 = new Promise((resolve) => setTimeout(resolve, 1000, 'Resolves after 1s'));
+let promise2 = new Promise((resolve) => setTimeout(resolve, 2000, 'Resolves after 2s'));
+let promise3 = new Promise((resolve, reject) => setTimeout(reject, 0));
+
+let promises = [promise1, promise2, promise3];
+
+Promise.any(promises)
+    .then(value => console.log(value)); // Resolves after 100ms
+
+(async () => {
+    try {
+        const output = await Promise.any([
+            Promise.reject('Error 1'),
+            Promise.reject('Error 2'),
+            Promise.reject('Error 3'),
+        ]);
+        console.log(`Output: ${output}`);
+    } catch (err) {
+        console.log(`Error: ${err.errors}`);
+    }
+})();
