7cc5411e814f63b71577911a7d94283e46d08d37
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-30 18:02:12 +0530

    Update PromiseAny.js

--- JavaScript/Advanced/PromiseAny.js
+++ JavaScript/Advanced/PromiseAny.js
@@ -1,3 +1,4 @@
+// Promise.any()
 
 let promise1 = new Promise((resolve) => setTimeout(resolve, 1000, 'Resolves after 1s'));
 let promise2 = new Promise((resolve) => setTimeout(resolve, 2000, 'Resolves after 2s'));
@@ -8,6 +9,7 @@ let promises = [promise1, promise2, promise3];
 Promise.any(promises)
     .then(value => console.log(value)); // Resolves after 100ms
 
+// Different Method: 
 (async () => {
     try {
         const output = await Promise.any([
