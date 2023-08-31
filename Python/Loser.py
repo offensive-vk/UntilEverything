1f8a702b427c72483b644013723e756d9b6336cd
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 16:57:09 +0530

    .py

---
+++ Python/Loser.py
@@ -0,0 +1,13 @@
+# Program to prove python is slow lol..
+
+import time
+start = time.time()
+print("\tProcessing...", start)
+
+n = 1
+while n < 1000000000:
+    n+=1
+    
+
+total = time.time() - start
+print("\tSeconds: ", total, "Result : ", n) 
