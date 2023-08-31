2ed16e7586b1c3d7c62fcc8159fcd15fbb5bce89
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 20:58:32 +0530

    fixes

---
+++ PHP/Overriding.php
@@ -0,0 +1,38 @@
+<?php 
+class Shape {
+    public function area() {
+        return 0; // Default implementation
+    }
+}
+
+class Circle extends Shape {
+    private $radius;
+    
+    public function __construct($radius) {
+        $this->radius = $radius;
+    }
+    
+    public function area() {
+        return pi() * pow($this->radius, 2);
+    }
+}
+
+class Square extends Shape {
+    private $side;
+    
+    public function __construct($side) {
+        $this->side = $side;
+    }
+    
+    public function area() {
+        return pow($this->side, 2);
+    }
+}
+
+$circle = new Circle(5);
+$square = new Square(4);
+
+echo "Circle Area: " . $circle->area() . "\n"; // Outputs: Circle Area: 78.539816339745
+echo "Square Area: " . $square->area() . "\n"; // Outputs: Square Area: 16
+
+?>
--- TypeScript/Quote.ts
+++ TypeScript/Quote.ts
@@ -135,3 +135,23 @@ console.log(...myLife);
 //     });
 
 // End
+
+// Starting lol
+// Profile Program 
+
+interface Life {
+    code: Promise<string | boolean>;
+    rest: Promise<string | boolean>;
+}
+
+const vedansh: Life = {
+    async code() {
+        return "< Writing code />";
+    },
+    async rest() {
+        return "< Resting />";
+    },
+};
+
+const myLife: Array<Promise<Life | string | boolean>> = [vedansh.code(), vedansh.rest(), vedansh.code()];
+console.log(myLife[0]);
