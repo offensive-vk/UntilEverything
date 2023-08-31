74a5e270788b6a987380a2cfff2bc54a961ae99f
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 18:05:15 +0530

    main grind

--- TypeScript/Quote.ts
+++ TypeScript/Quote.ts
@@ -3,16 +3,100 @@
 interface Life {
     code: Promise<string | boolean>;
     rest: Promise<string | boolean>;
+    learn: Promise<string | boolean>;
 }
-
+interface Wasted{
+    wasted: Promise<string | boolean>;
+    again: Array<Promise<Life>>;
+}
 const vedansh: Life = {
     async code() {
-        return "< Writing code />";
+        return "->< Writing code />";
     },
     async rest() {
-        return "< Resting />";
+        return "->< Resting />";
     },
+    async learn(){
+        return "->< Learning />";
+    }
 };
 
-const myLife: Array<Promise<Life | string | boolean>> = [vedansh.code(), vedansh.rest(), vedansh.code()];
-console.log(myLife[0]);
+const myLife: Array<Promise<Array<Life> | Array<Wasted>>> = [vedansh.code(), vedansh.rest(), vedansh.code()];
+console.log(...myLife);
+
+// Another - 2
+// interface Life {
+//     code: Promise<string | boolean>;
+//     rest: Promise<string | boolean>;
+//     learn: Promise<string | boolean>;
+// }
+
+// const vedansh: Life = {
+//     async code() {
+//         return "< Writing code />";
+//     },
+//     async rest() {
+//         return "< Resting />";
+//     },
+//     async learn() {
+//         return "< Learning new things />";
+//     },
+// };
+
+// const myLifeLoop = async () => {
+//     while (true) {
+//         const activities: Array<Promise<string | boolean>> = [
+//             vedansh.code(),
+//             vedansh.rest(),
+//             vedansh.learn(),
+//         ];
+
+//         for (const activity of activities) {
+//             console.log(await activity);
+//         }
+//     }
+// };
+
+// myLifeLoop();
+
+// // Another - 3
+// interface Life {
+//     code: Promise<string>;
+//     rest: Promise<string>;
+//     learn: Promise<string>;
+// }
+
+// const vedansh: Life = {
+//     async code() {
+//         return "< Writing code />";
+//     },
+//     async rest() {
+//         return "< Resting />";
+//     },
+//     async learn() {
+//         return "< Learning new things />";
+//     },
+// };
+
+// const myLifeLoop = async () => {
+//     while (true) {
+//         const activities: Array<Promise<Life | boolean>> = [
+//             vedansh.code(),
+//             vedansh.rest(),
+//             vedansh.learn(),
+//         ];
+
+//         for (const activityPromise of activities) {
+//             const activity = await activityPromise;
+            
+//             if (typeof activity === 'boolean') {
+//                 console.log('Got a boolean:', activity);
+//             } else {
+//                 console.log('Got a Life activity:', activity);
+//             }
+//         }
+//     }
+// };
+
+// myLifeLoop();
+
