c5da38a001d7ac92ccf5172c585ad4745556f5d8
offensive-vk <121493131+offensive-vk@users.noreply.github.com>
2023-08-31 21:27:37 +0530

    php

--- PHP/Interfaces.php
+++ PHP/Interfaces.php
@@ -16,13 +16,18 @@ class Manish implements Teacher{
 }
 class AnotherTeacher extends Manish implements Teacher{
     public $isExpert = false;
+    private $salary = 0;
     public function __construct($name, $id, $Expert){
         parent::__construct($name, $id);
         $this->isExpert = $Expert;
+        $this->salary += 0;
         echo "Values Taken from Parent Constructor" ;
     }
-    
+    public function getSalary(): void{
+        echo "Your Salary: ", $this->salary + (30*8000-5000);
+    }
 }
+// Parent Class
 $teacher = new Manish("Manish", 9001);
 echo $teacher->getName();
 $teacher->teach();
