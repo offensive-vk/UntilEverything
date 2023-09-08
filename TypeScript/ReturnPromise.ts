0cff927ed6b1fc3821c95c1cc3115f2398cf8d30
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-08 13:51:11 +0530

    tsx

--- TypeScript/ReturnPromise.ts
+++ TypeScript/ReturnPromise.ts
@@ -14,3 +14,27 @@ async function asyncPromise(): Promise<number> {
     return result.length; // Returns the length of the resolved string
 }
 
+// Using the promises
+resolvedPromise()
+    .then((result) => {
+        console.log(result); // This is a resolved promise.
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
+
+rejectedPromise()
+    .then((result) => {
+        // This block won't be executed due to the rejection.
+    })
+    .catch((error) => {
+        console.error("Error: " + error.message); // Error: This is a rejected promise.
+    });
+
+asyncPromise()
+    .then((result) => {
+        console.log("Length of resolved promise:", result); // Length of resolved promise: 24
+    })
+    .catch((error) => {
+        console.error(error.message);
+    });
