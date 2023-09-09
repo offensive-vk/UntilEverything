3878731e789ccd859ad7cdc33e2634178a6cb8fd
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-09 13:56:27 +0530

    ts

---
+++ TypeScript/Awaited.ts
@@ -0,0 +1,21 @@
+// Awaited type
+type Awaited<T> = T extends Promise<infer R> ? R : T;
+
+// Test cases
+type A = Awaited<Promise<string>>;
+// type A = string
+
+type B = Awaited<Promise<Promise<number>>>;
+// type B = number
+
+type C = Awaited<boolean | Promise<number>>;
+// type C = number | boolean
+
+// Using the types
+const aValue: A = "Hello, World!"; // Valid assignment
+const bValue: B = 42; // Valid assignment
+const cValue: C = true; // Valid assignment
+
+console.log(aValue);
+console.log(bValue);
+console.log(cValue);
