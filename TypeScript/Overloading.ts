f4d29d1bd7483e981bd4499f6a40c065956aab2d
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-09 13:47:20 +0530

    fun

---
+++ TypeScript/Overloading.ts
@@ -0,0 +1,19 @@
+// Function overloads
+function greet(person: string): string;
+function greet(person: string, age: number): string;
+
+// Implementation of the overloaded function
+function greet(person: string, age?: number): string {
+    if (age === undefined) {
+        return `Hello, ${person}!`;
+    } else {
+        return `Hello, ${person}! You are ${age} years old.`;
+    }
+}
+
+// Using the overloaded function
+const greeting1 = greet("Alice"); 
+const greeting2 = greet("Bob", 30);
+
+console.log(greeting1);
+console.log(greeting2); 
