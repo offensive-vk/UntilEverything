9cfbb6e7326d2103e48ad9992a3e00328cfbc5a2
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-12 09:26:05 +0530

    Imp

--- TypeScript/Generic.ts
+++ TypeScript/Generic.ts
@@ -1,11 +1,25 @@
 class Global<T>{
     private value: T | undefined;
     private reg: T | undefined;
+    static check: boolean | string;
+
+    constructor (
+        public id: number,
+        public salary: number,
+        public name: string
+    ) {}
+
+    public showPrivateData(): void{
+        console.log(`${this.value}\n ${this.reg}\n`);
+    }
+    public showPublicData(): void{
+        console.log(`${this.id}\n ${this.salary}\n ${this.name}\n`);
+    }
 }
 
 
-function Sample<T>(anything?: any): T {
-    return arguments[0];
+function Sample<T>(anything?: T): T {
+    return anything;
 }
 
 let _sample = Sample<string>('TypeScript');
