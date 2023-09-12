72e6b8b2f57aebb08c16580f577476ed7f337d4e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-12 09:17:08 +0530

    Imp

--- TypeScript/Generic.ts
+++ TypeScript/Generic.ts
@@ -2,3 +2,11 @@ class Global<T>{
     private value: T | undefined;
     private reg: T | undefined;
 }
+
+
+function Sample<T>(anything?: any): T {
+    return arguments[0];
+}
+
+let _sample = Sample<string>('TypeScript');
+console.log(_sample);
