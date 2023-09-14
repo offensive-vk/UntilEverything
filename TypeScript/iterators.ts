feed775fe07c7d7923b4be36f3abd861a3a577b1
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-14 20:29:21 +0530

    ~

---
+++ TypeScript/iterators.ts
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3, 4, 5];
+
+// Create an iterator from the array
+const iterator = numbers[Symbol.iterator]();
+
+// Iterate over the array using the iterator
+console.log("Using iterator in TypeScript:");
+let result = iterator.next();
+while (!result.done) {
+    console.log(result.value);
+    result = iterator.next();
+}
