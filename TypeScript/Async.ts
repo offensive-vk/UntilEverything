10871cc84be5fe247d2109e72514a31543544924
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-19 22:17:57 +0530

    Final

---
+++ TypeScript/Async.ts
@@ -0,0 +1,25 @@
+function fetchData(): Promise<string> {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Data fetched successfully');
+        }, 2000);
+    });
+}
+
+// Define an async function to fetch and process data
+async function fetchAndProcessData() {
+    console.log('Fetching data...');
+
+    try {
+        const data = await fetchData(); // Use 'await' to wait for the Promise to resolve
+        console.log('Received data:', data);
+        console.log('Processing data...');
+        // Simulate further processing
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.log('Data processing complete');
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+fetchAndProcessData();
