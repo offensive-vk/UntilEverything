8c2ef2f980d25484ae6ddf95003df0cb49886523
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-20 18:57:40 +0530

    ~

--- Node/Buffer.js
+++ Node/Buffer.js
@@ -1,9 +1,12 @@
 const Buffer = require('buffer').Buffer;
+var Arr = [54,547,73,8,458,48];
 
 var Buf = Buffer.alloc(128);
-
 Buf.write('This is a buffer');
+Buf.write('Bruh another buffer');
+Buf.fill(Arr);
 
-
+console.log(Buffer.from('This is another buffer').toLocaleString());
 console.log(Buf.length);
 console.log(Buf.toString('UTF-8'));
+console.log(Buf)
