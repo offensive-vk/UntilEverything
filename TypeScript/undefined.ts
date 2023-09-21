042fada1a7953364cf78177a85d48209633b0004
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-21 16:47:44 +0530

    ts

---
+++ TypeScript/undefined.ts
@@ -0,0 +1,6 @@
+async function main<T>(x: number | T, y: T | number, ...args: (unknown | undefined)[]): Promise<T[]> {
+    // Your asynchronous function logic here
+
+    const resultArray: T[] = [];
+    return resultArray;
+}
