b93d5e268a63c6310f7928667cfc3570c25b2361
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-21 16:50:25 +0530

    ~~~

--- TypeScript/undefined.ts
+++ TypeScript/undefined.ts
@@ -10,3 +10,22 @@ main<number>(42, 56).then(result => {
 });
 
 /* More Detailed */
+async function _main<T>(x: number | T, y: T | number, ...args: (unknown | undefined)[]): Promise<T[]> {
+    
+    await new Promise(resolve => setTimeout(resolve, 1500)); 
+
+    // Create and return an array of type T
+    const resultArray: T[] = [x, y, ...args.filter(arg => typeof arg !== 'undefined')] as T[];
+    return resultArray;
+}
+
+async function exampleUsage() {
+    try {
+        const result: number[] = await _main<number>(42, 56, undefined, 70);
+        console.log(result);
+    } catch (error) {
+        console.error('An error occurred:', error);
+    }
+}
+
+exampleUsage();
