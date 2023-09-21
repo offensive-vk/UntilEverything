ee60c57a17bd04d6618bcee44f2d884408c9cb2e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-21 16:39:47 +0530

    post

--- Node/Requests.js
+++ Node/Requests.js
@@ -13,3 +13,31 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
         console.error('Error:', error);
     });
 
+
+// Sending a POST request with JSON data
+const postData = {
+    userId: 1,
+    id: 101,
+    title: 'foo',
+    body: 'bar'
+};
+
+fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+})
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
