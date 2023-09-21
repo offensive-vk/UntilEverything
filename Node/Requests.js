08741a76c5bf67a5301f7c343ebf5295b795a2e8
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-21 16:38:19 +0530

    ~

---
+++ Node/Requests.js
@@ -0,0 +1,15 @@
+// Sending a GET request
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
