d5a12562573dba131427a9eeb545989c6f272402
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-22 22:41:48 +0530

    ~~

--- TypeScript/decorator.ts
+++ TypeScript/decorator.ts
@@ -2,7 +2,9 @@
 Decorators are a feature of TypeScript that allow you to modify the behavior of a class, property, method, or parameter. They are a way to add additional functionality to existing code, and they can be used for a wide range of tasks, including logging, performance optimization, and validation.
 
 */
-
+function sayHi(target: Function){
+    setTimeout(() => console.log(`Hi ${target.name} is used.`), 1500);
+}
 // Decorator function
 function logClass(target: Function) {
     console.log(`Class ${target.name} is instantiated.`);
@@ -13,4 +15,4 @@ function logClass(target: Function) {
 class ExampleClass { }
 
 // Creating an instance of the decorated class
-const exampleInstance = new ExampleClass();
+const Example = new ExampleClass();
