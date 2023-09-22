84ae185458f8acdc180a48eeab6f06698a631340
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-22 22:33:41 +0530

    @@

---
+++ TypeScript/decorator.ts
@@ -0,0 +1,4 @@
+/* 
+Decorators are a feature of TypeScript that allow you to modify the behavior of a class, property, method, or parameter. They are a way to add additional functionality to existing code, and they can be used for a wide range of tasks, including logging, performance optimization, and validation.
+
+*/
