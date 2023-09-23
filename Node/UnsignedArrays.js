c29d58656d004e8e228aac21efa4549c2ed0af9e
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-23 17:00:08 +0530

    arrays

---
+++ Node/UnsignedArrays.js
@@ -0,0 +1,23 @@
+// Uint8Array
+const uint8Array = Uint8Array.of(10, 20, 30, 40);
+console.log(uint8Array); // Output: Uint8Array [ 10, 20, 30, 40 ]
+
+// Uint16Array
+const uint16Array = Uint16Array.of(100, 200, 300);
+console.log(uint16Array); // Output: Uint16Array [ 100, 200, 300 ]
+
+// Uint32Array
+const uint32Array = Uint32Array.of(1000, 2000);
+console.log(uint32Array); // Output: Uint32Array [ 1000, 2000 ]
+
+// Uint8ClampedArray
+const uint8ClampedArray = Uint8ClampedArray.of(300, 500, 700);
+console.log(uint8ClampedArray); // Output: Uint8ClampedArray [ 255, 255, 255 ]
+
+// Float32Array
+const float32Array = Float32Array.of(3.14, 2.718, 1.618);
+console.log(float32Array); // Output: Float32Array [ 3.14, 2.718, 1.618 ]
+
+// Float64Array
+const float64Array = Float64Array.of(3.14159, 2.71828);
+console.log(float64Array); // Output: Float64Array [ 3.14159, 2.71828 ]
