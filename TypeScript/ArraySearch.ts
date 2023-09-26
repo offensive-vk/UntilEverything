a6e7135d1fb9c8e0423a514adb0d8adddc541f1b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-26 18:29:59 +0530

    dsa

--- TypeScript/ArraySearch.ts
+++ TypeScript/ArraySearch.ts
@@ -1,19 +1,19 @@
-function linearSearch(arr: number[], target: number): number {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === target) {
+function linearSearch(Arr: number[], target: number): number {
+    for (let i = 0; i < Arr.length; i++) {
+        if (Arr[i] === target) {
             return i; // Return the index of the found element
         }
     }
     return -1; // Return -1 if the element is not found
 }
-function binarySearch(arr: number[], target: number): number {
+function binarySearch(Arr: number[], target: number): number {
     let left = 0;
-    let right = arr.length - 1;
+    let right = Arr.length - 1;
     while (left <= right) {
         const mid = Math.floor((left + right) / 2);
-        if (arr[mid] === target) {
+        if (Arr[mid] === target) {
             return mid; // Return the index of the found element
-        } else if (arr[mid] < target) {
+        } else if (Arr[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -21,3 +21,17 @@ function binarySearch(arr: number[], target: number): number {
     }
     return -1; // Return -1 if the element is not found
 }
+
+const Arr = [1, 2, 3, 4, 5];
+
+// indexOf: Returns the index of the first occurrence of an element, or -1 if not found
+const index = Arr.indexOf(3);
+
+// includes: Returns true if an element is found in the array, false otherwise
+const hasValue = Arr.includes(4);
+
+// find: Returns the first element that satisfies a condition, or undefined if not found
+const foundValue = Arr.find((item) => item === 2);
+
+// findIndex: Returns the index of the first element that satisfies a condition, or -1 if not found
+const foundIndex = Arr.findIndex((item) => item === 5);
