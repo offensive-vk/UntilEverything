958e8a25efe4e716c7c4f904a42cd505c0f8fc0d
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-26 18:29:16 +0530

    dsa

--- TypeScript/ArraySearch.ts
+++ TypeScript/ArraySearch.ts
@@ -6,3 +6,18 @@ function linearSearch(arr: number[], target: number): number {
     }
     return -1; // Return -1 if the element is not found
 }
+function binarySearch(arr: number[], target: number): number {
+    let left = 0;
+    let right = arr.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (arr[mid] === target) {
+            return mid; // Return the index of the found element
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1; // Return -1 if the element is not found
+}
