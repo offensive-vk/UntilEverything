1d2d4fe5ce9c7a42d1a9a4c4528b2860ac368c9f
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-28 17:37:01 +0530

    ts

--- TypeScript/main.tsx
+++ TypeScript/main.tsx
@@ -1,5 +1,26 @@
 export function info(...args: any[]): void{
     console.log(`[info] >> [ ${args} ] << [info]`);
 }
+export function generatePlayerId(): Promise<string | void> | string | null {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    let numCount = 0;
+    let charCount = 0;
+    for (let i = 0; i < 10; i++) {
+        if (numCount < 7 || (numCount >= 7 && charCount < 3 && Math.random() < 0.5)) {
+            result += characters.charAt(Math.floor(Math.random() * characters.length));
+            if (/[0-9]/.test(result.charAt(result.length - 1))) {
+                numCount++;
+            } else {
+                charCount++;
+            }
+        } else {
+            result += characters.charAt(Math.floor(Math.random() * 26) + 26);
+            charCount++;
+        }
+    }
+    return result ? result : null;
+}
 
-export default main
+const random: any = generatePlayerId();
+info(random);
