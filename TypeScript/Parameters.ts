899668d8ab385b287879c559e8b4e13901ae7ce4
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-29 14:49:25 +0530

    ts+

--- TypeScript/Parameters.ts
+++ TypeScript/Parameters.ts
@@ -3,3 +3,20 @@ Parameters constructs a tuple type from the types used in the parameters of a fu
 OR
 The Parameters<Type> utility type in TypeScript is used to extract the parameter types of a function or constructor type. 
 */
+// Define a sample function
+function greet(name: string, age: number): void {
+    console.log(`Hello, ${name}! You are ${age} years old.`);
+}
+
+// Extract parameter types using Parameters<>
+type GreetParams = Parameters<typeof greet>;
+
+// Define a function that takes the parameter types and logs them
+function logParameters(params: GreetParams): void {
+    const [name, age] = params;
+    console.log(`Parameter types: name (${typeof name}), age (${typeof age})`);
+}
+
+// Call the logParameters function with the parameter types
+const params: GreetParams = ['Alice', 30];
+logParameters(params);
