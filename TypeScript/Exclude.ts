b0b482405971cfc525160c9487a55aa55c1e877a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-29 14:35:34 +0530

    ts+

---
+++ TypeScript/Exclude.ts
@@ -0,0 +1,5 @@
+/* 
+Exclude constructs a type by excluding from UnionType all union members that are assignable to ExcludedMembers.
+The Exclude<Type, ExcludedUnion> utility type in TypeScript is used to create a new type by excluding types from another type. It removes types from Type that are assignable to ExcludedUnion.
+
+*/
