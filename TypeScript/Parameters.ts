57f45188871a1cc8db465e41f14e8e284ee2ede7
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-29 14:46:53 +0530

    ts+

---
+++ TypeScript/Parameters.ts
@@ -0,0 +1,5 @@
+/* 
+Parameters constructs a tuple type from the types used in the parameters of a function type Type.
+OR
+The Parameters<Type> utility type in TypeScript is used to extract the parameter types of a function or constructor type. 
+*/
