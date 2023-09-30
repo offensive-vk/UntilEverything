3f0609580898094fbd19a2b9f44fea167a47d744
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-09-30 13:50:35 +0530

    ts+

---
+++ TypeScript/EscapeSeq.ts
@@ -0,0 +1,24 @@
+function sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function animate() {
+    const frames = ['|', '/', '-', '\\']; // Frames for animation
+    let currentFrameIndex = 0;
+
+    while (true) {
+        // Clear the console (works in Node.js)
+        console.clear();
+
+        // Output the current frame
+        console.log(frames[currentFrameIndex]);
+
+        // Move to the previous frame or wrap around
+        currentFrameIndex = (currentFrameIndex + 1) % frames.length;
+
+        // Wait for a short period (e.g., 100 milliseconds)
+        await sleep(100);
+    }
+}
+
+animate();
