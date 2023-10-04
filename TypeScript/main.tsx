ad5f17e543ac887fbc2fa42439065a71db72d1b0
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-04 16:23:44 +0530

    ...

--- TypeScript/main.tsx
+++ TypeScript/main.tsx
@@ -6,15 +6,13 @@ export function info(...args: any[]): void{
     console.log(`[info] >> [ ${args} ] << [info]`);
 }
 /**
- * Logs a formatted message to the console.
- * 
+ * Logs a traced message to the console.
  * @param args An array of arguments that will be logged to the console.
- * 
  * @example
  * _info("This is a log message");
  * // Logs: "YYYY-MM-DD HH:MM:SS.MMM AM/PM [info] >> [ 'This is a log message' ] << [info]"
  */
-function _info(...args: any[]): void {
+function trace(...args: any[]): void {
     const currentDate = new Date();
     const hours = currentDate.getHours() > 12 ? currentDate.getHours() - 12 : currentDate.getHours();
     const minutes = currentDate.getMinutes() < 10 ? `0${currentDate.getMinutes()}` : currentDate.getMinutes();
