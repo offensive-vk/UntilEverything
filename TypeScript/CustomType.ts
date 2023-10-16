33b6561bbc23c1c3c2ab0762acfb9e9688535d78
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-16 15:02:08 +0530

    Custom

---
+++ TypeScript/CustomType.ts
@@ -0,0 +1,22 @@
+type ExtractPropertiesOfType<T, U> = {
+    [K in keyof T as T[K] extends U ? K : never]: T[K];
+};
+
+// Example usage with additional improvements
+type Item = {
+    name: string;
+    age: number;
+    address: string;
+    email: string | null;
+};
+
+type ExtractStringProperties<T> = ExtractPropertiesOfType<T, string>;
+type ExtractNumericProperties<T> = ExtractPropertiesOfType<T, number>;
+
+// Extract string properties from the 'Person' type
+type StringProperties = ExtractStringProperties<Item>;
+// Extract numeric properties from the 'Person' type
+type NumericProperties = ExtractNumericProperties<Item>;
+
+// StringProperties: { name: string, address: string, email: string | null }
+// NumericProperties: { age: number }
