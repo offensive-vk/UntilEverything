8cafdc34dc469a86a378e59ef0f4572effeedfd4
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-19 15:18:19 +0530

    <T>

---
+++ TypeScript/AddSome.ts
@@ -0,0 +1,17 @@
+function addRandomId<T>(input: T): T & { id: string } {
+    const randomId = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
+    return {
+        ...input,
+        id: randomId,
+    };
+}
+
+// Example usage:
+const person = { name: 'John', age: 30 };
+const product = { name: 'Widget', price: 19.99 };
+
+const personWithId = addRandomId(person);
+const productWithId = addRandomId(product);
+
+console.log(personWithId); // { name: 'John', age: 30, id: '1234' } (ID will be different)
+console.log(productWithId); // { name: 'Widget', price: 19.99, id: '5678' } (ID will be different)
