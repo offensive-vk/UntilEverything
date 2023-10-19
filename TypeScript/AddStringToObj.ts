3295c384f446a23fd73e3e7b7d6e487e806312f2
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-10-19 15:24:17 +0530

    ~

---
+++ TypeScript/AddStringToObj.ts
@@ -0,0 +1,15 @@
+function appendStringToProperty<T, K extends keyof T>(obj: T, prop: K, appendString: string): T {
+    const updatedObj = { ...obj };
+    updatedObj[prop] = updatedObj[prop] + appendString as T[K];
+    return updatedObj;
+}
+
+// Example usage:
+const Banana = { name: 'John', age: 30 };
+const Item = { name: 'Widget', price: 19.99 };
+
+const personWithSuffix = appendStringToProperty(Banana, 'name', ' Doe');
+const productWithSuffix = appendStringToProperty(Item, 'name', ' Plus');
+
+console.log(personWithSuffix); // { name: 'John Doe', age: 30 }
+console.log(productWithSuffix); // { name: 'Widget Plus', price: 19.99 }
