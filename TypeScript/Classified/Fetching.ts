02cca4dd837091ac063914349ea9865b8e20e033
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-02 13:47:48 +0530

    Finales

--- TypeScript/Classified/Fetching.ts
+++ TypeScript/Classified/Fetching.ts
@@ -14,11 +14,12 @@ type UserData<T extends number, U extends string> = { userid: T, username: U };
  * @param callback extra error handling funtion which is optional in nature.
  * @returns {UserData} as primary output.
  */
-function fetchUserData<Type>(url: Type | string, casualname: string , callback?: () => {number, string}): UserData<number, string>{
-    const server: unknown = fetch("https://getuserstats.org/");
+function fetchUserData<Type>(url: Type | string, casualname: string , callback?: () => void | Object): UserData<number, string>{
+    const server: unknown = fetch("https://getuserstats.org/temp/");
     var userid: number | undefined = Math.floor(Math.random() * 99999);
     var username: string | undefined = `${casualname}`.concat("@override.ps1");
-    over.trace(server);    
+    over.trace(server);
+    
     return {userid, username} as UserData<number, string>;
 }
 
