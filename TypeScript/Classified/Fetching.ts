50e584d884f01136e65fc07d39adc038b00b08b3
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-07 13:37:05 +0530

    ~~

--- TypeScript/Classified/Fetching.ts
+++ TypeScript/Classified/Fetching.ts
@@ -21,19 +21,19 @@ type UserData<T extends number, U extends string> = { userid: T, username: U };
  * @returns {UserData} as primary output like key value pairs.
  */
 function fetchUserData<Type>(
-        url: Type | string, 
-        casualname: Type | string , 
-        callback?: (err: Error) => void | Object
-    ): UserData<number, string>{
-    var server: unknown = url? fetch(url as string) : fetch("https://getuserstats.org/temp/");
-    var userid: number | undefined = Math.floor(Math.random() * 99999);
-    var username: string | undefined = `${casualname}`.concat("@override.ps1");
-    // trace(server);
+    url: Type | string, 
+    casualname: Type | string , 
+    callback?: (err: Error) => void | Object
+): UserData<number, string>{
+var server: unknown = url? fetch(url as string) : fetch("https://getuserstats.org/temp/");
+var userid: number | undefined = Math.floor(Math.random() * 99999);
+var username: string | undefined = `${casualname}`.concat("@override.ps1");
+// trace(server);
 
-    const FINAL_DATA = {"UserID ": userid, "Username ": username};
-    console.dir(FINAL_DATA);
+const FINAL_DATA = {"UserID ": userid, "Username ": username};
+console.dir(FINAL_DATA);
 
-    return {userid, username} as UserData<number, string>;
+return {userid, username} as UserData<number, string>;
 }
 
 /**
