e423d1f7dd4ba8a4d5f2011a7e1290765c1b855d
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-08 12:22:27 +0530

    Adv

--- TypeScript/AdvUtility.ts
+++ TypeScript/AdvUtility.ts
@@ -2,3 +2,13 @@
 function isType<T extends { [key: string]: any }>(input: any, constructor: new (...args: any[]) => T): input is T {
     return input instanceof constructor;
 }
+// Now, let's use these functions to validate type parameters in our generic function
+function performAction<T extends { [key: string]: any }>(input: any, constructor: new (...args: any[]) => T): T {
+    if (isType<T>(input, constructor)) {
+        // If the input value satisfies the constraint, we perform the action and return the result
+        return input;
+    } else {
+        throw new Error('Input does not satisfy the type constraint');
+    }
+}
+
