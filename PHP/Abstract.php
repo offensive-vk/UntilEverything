a50285d7b936a124eb6e981ccfbc14bb0f08f828
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-09 18:53:40 +0530

    abstract

--- PHP/Abstract.php
+++ PHP/Abstract.php
@@ -1,5 +1,6 @@
 <?php 
-echo "== Abstraction is PHP. == \n";
+echo "== Abstraction in PHP. == \n";
+
 /**
  * @abstract class Container 
  * @global Container class for all child classes.
@@ -15,29 +16,38 @@ abstract class Container {
      * @abstract method addItem();
      */
     abstract function addItem($item);
+
     /**
      * @abstract method getLength();
      */
     abstract function getLength();
+
     /**
      * @abstract method getItem($index);
      */
     abstract function getItem($index);
 }
+
 class ParentContainer extends Container {
-    public function addItem($item){
-        $this->items[] = array_push($this->items, $item);
+    public function __construct() {
+        $this->items = [];
     }
+
+    public function addItem($item){
+        array_push($this->items, $item);
+    }
+
     /**
      * @abstract method getLength();
      */
     public function getLength(){
-        echo "The Length of Items is : ".count($this->items);
         return count($this->items);
     }
+
     public function getArray() {
         return $this->items;
     }
+
     /**
      * @abstract method getItem($index);
      */
@@ -45,14 +55,15 @@ class ParentContainer extends Container {
 
     }
 }
+
 $num = new ParentContainer();
 $num->addItem(5000);
 $num->addItem(6000);
 $num->addItem(7000);
 $num->addItem(8000);
-$num->getLength();
+
 $arr = $num->getArray();
 for($i = 0; $i < $num->getLength(); $i++){
-    echo $arr[$i] ."";
+    echo $arr[$i] . " ";
 }
 ?>
