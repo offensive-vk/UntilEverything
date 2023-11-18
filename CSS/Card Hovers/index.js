048e8225faf8d61162b00d61bbf9dd093453c254
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-11-18 15:22:55 +0530

    ~

---
+++ CSS/Card Hovers/index.js
@@ -0,0 +1,18 @@
+// const fs = require('fs');
+import fs from 'fs';
+document.querySelector(".btn").addEventListener("click", () => {
+    var oldValue = '';
+    fs.readFile('index.css', 'utf-8', (err, data) => {
+        if(err) throw err;
+        oldValue = data;
+    });
+    var newValue = '';
+    fs.readFile('card.css', 'utf-8', (err, data) => {
+        if(err) throw err;
+        newValue = data;
+    });
+
+    fs.writeFile('index.css', newValue, 'utf-8', (err) => {
+        if (err) throw err;
+    });
+})
