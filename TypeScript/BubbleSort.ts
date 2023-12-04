d453ed1c947e9c6b65379cc889425128505ce38b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2023-12-04 21:31:29 +0530

    sort

---
+++ TypeScript/BubbleSort.ts
@@ -0,0 +1,24 @@
+function bubbleSort(arr: number[]): number[] {
+    const n = arr.length;
+
+    // Outer loop - controls the number of passes
+    for (let i = 0; i < n - 1; i++) {
+        // Inner loop - compares and swaps elements
+        for (let j = 0; j < n - 1 - i; j++) {
+            // Compare adjacent elements
+            if (arr[j] > arr[j + 1]) {
+                // Swap if they are in the wrong order
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+            }
+        }
+    }
+
+    return arr;
+}
+
+// Example usage:
+const unsortedArray = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(unsortedArray.slice());
+
+console.log("Unsorted Array:", unsortedArray);
+console.log("Sorted Array:", sortedArray);
