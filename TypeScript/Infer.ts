ca118c402159c21d126281719977e7701ebc9225
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-08 10:14:20 +0530

    Infer<>

---
+++ TypeScript/Infer.ts
@@ -0,0 +1,3 @@
+/*
+Conditional types in TypeScript allow you to express non-uniform types based on a condition. The infer keyword is typically used in the extends clause of a conditional type to capture and infer a type from another type.
+*/
