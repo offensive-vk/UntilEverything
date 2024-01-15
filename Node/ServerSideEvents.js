0d305a916f44261d9a0280ad0e3c8893aa6c079a
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-01-15 10:57:07 +0530

    Finales

--- Node/ServerSideEvents.js
+++ Node/ServerSideEvents.js
@@ -27,3 +27,13 @@ server.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
 
+{/* <script> */}
+const eventSource = new EventSource('http://localhost:3000/events');
+
+eventSource.onmessage = (event) => {
+  console.log('Received event:', event.data);
+  // Display the message on the page, for example:
+  document.getElementById('time').textContent = event.data;
+};
+{/* </script> */}
+{/* <p id="time"></p> */}
