d17e55cea1fa31d6bf006b2460724f9541e33200
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-03 12:37:31 +0530

    HTTP

---
+++ Ajax/HTTP.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AJAX HTTP Methods Example</title>
+
+</head>
+<body>
+    <h2>AJAX HTTP Methods Example</h2>
+    <button onclick="getMethod()">GET Request</button>
+    <button onclick="postMethod()">POST Request</button>
+    <button onclick="putMethod()">PUT Request</button>
+    <button onclick="deleteMethod()">DELETE Request</button>
+</body>
+</html>
