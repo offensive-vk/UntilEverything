422774fe6db47fd4f129292af6f0b94576293f35
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-03 12:38:55 +0530

    HTTP

--- Ajax/HTTP.php
+++ Ajax/HTTP.php
@@ -4,7 +4,25 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>AJAX HTTP Methods Example</title>
+<script>
+    function makeRequest(method, url, data, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 300) {
+            callback(null, xhr.responseText);
+            } else {
+            callback(new Error('Request failed: ' + xhr.statusText));
+            }
+        }
+        };
+        xhr.open(method, url, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(data));
+    }
 
+
+</script>
 </head>
 <body>
     <h2>AJAX HTTP Methods Example</h2>
