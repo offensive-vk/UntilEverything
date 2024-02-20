112793bef7e6863d48b259f9b858df8d25667b8b
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-02-20 11:59:40 +0530

    Basics

--- JavaScript/Basic/objectBasis.js
+++ JavaScript/Basic/objectBasis.js
@@ -8,11 +8,12 @@ console.log(person.name);
 console.log(person.age);
 console.log(person.profession);
 
-//display by another method
+// display by another method
 
 for (let key in person) {
     console.log(key + ": " + person[key]);
 }
+
 // to add a property in Object :
 person.salary = 95000;
 console.log(person);
@@ -20,4 +21,3 @@ console.log(person);
 //to delete a property in Object:
 delete person.age;
 console.log(person);
-
