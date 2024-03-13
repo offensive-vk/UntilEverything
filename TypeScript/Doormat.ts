e6584a4fe2ffb96ad5475eb30b8563b7a1768e88
Vedansh ✨ <121493131+offensive-vk@users.noreply.github.com>
2024-03-13 08:23:12 +0530

    One Last Time.
    Cool Functions.
    
    Co-Authored-By: node_module [bot] <161195843+npm-run-test@users.noreply.github.com>

---
+++ TypeScript/Doormat.ts
@@ -0,0 +1,14 @@
+function printWelcomePattern(N: number, M: number): void {
+    for (let i = 1; i < N; i += 2) {
+        console.log(".".repeat(i).concat("|").repeat(1).concat(".").repeat(i).padStart(M / 2 + i / 2, "-").padEnd(M, "-"));
+    }
+    console.log("WELCOME".padStart((M - 7) / 2 + 7 / 2, "-").padEnd(M, "-"));
+    for (let i = N - 2; i >= 1; i -= 2) {
+        console.log(".".repeat(i).concat("|").repeat(1).concat(".").repeat(i).padStart(M / 2 + i / 2, "-").padEnd(M, "-"));
+    }
+}
+
+// Example usage:
+const N: number = 10;
+const M: number = 10;
+printWelcomePattern(N, M);
